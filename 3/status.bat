17fc0cb34d96fd2bc28f221cfece3de3ddb58dc0
Dead4Life2 <Jonas110106@gmail.com>
2022-05-24 21:59:18 +0200

    test

--- 3/status.bat
+++ 3/status.bat
@@ -1,1 +1,13 @@
-D:/Programming/Education/Juniorstudium/5/status.bat
+@echo off
+set "cmd=%~1"
+set "params=%~2"
+
+if "%cmd%"=="" (
+	echo "status" executes a command and prints its exit code to the console
+	echo Usage: status ^<command^> "[command parameters]"
+	echo.
+	goto :eof
+) else (
+	start %cmd% "%params%"
+	echo.   Command "%cmd%" exited with Exit Code %errorLevel%
+	)
